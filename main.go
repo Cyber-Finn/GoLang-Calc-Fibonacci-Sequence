2ac458fb664675f344d3e33a85b759885d4052d8
CyberFinn <stephendiavolo@gmail.com>
2024-04-19 18:19:14 +0300

    Fix: Fibonacci sequence calculation Description: The Fibonacci sequence hsa some reiteration at the start - in "0,1,1" - we now cater for this and display the proper Fibonacci sequence

--- main.go
+++ main.go
@@ -19,7 +19,8 @@ func main() {
 
 func init_struct() {
 	continue_operation = true
-	fib_num.current_number = 1
+	fib_num.current_number = 0
+	print_data()
 }
 
 func calc_next_num() {
@@ -35,9 +36,14 @@ func print_data() {
 }
 
 func increment_data() bool {
-	if fib_num.current_number < 130 { //number here is arb/adjustable, currently prints: "1,2,3,5,8,13,21,34,55,89,144"
+	if fib_num.current_number < 130 { //number here is arb/adjustable, currently prints: "0,1,1,2,3,5,8,13,21,34,55,89,144,"
 		temp_num := 0
-		temp_num = fib_num.current_number + fib_num.previous_number
+		if fib_num.current_number == 0 {
+			temp_num = fib_num.current_number + 1
+		} else {
+			temp_num = fib_num.current_number + fib_num.previous_number
+		}
+
 		fib_num.previous_number = fib_num.current_number
 		fib_num.current_number = temp_num
 		return true
